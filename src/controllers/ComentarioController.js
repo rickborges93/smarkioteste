7f0a4f3ce1038331bfdac8ed4f081978844d9066
Henrique Borges <henriquesamensari@gmail.com>
2021-01-07 13:15:25 -0300

    Configurando sistema de controllers e services.

--- src/controllers/ComentarioController.js
+++ src/controllers/ComentarioController.js
@@ -1,5 +1,17 @@
+const ComentarioService = require('../services/ComentarioService');
+
 module.exports = {
     server: (req, res) => {
         res.json({server: true});
-    }
+    },
+
+    all: (req, res) => {
+        res.json({all: true});
+    },
+    new: (req, res) => {
+        res.json({new: true});
+    },
+    listen: (req, res) => {
+        res.json({listen: true});
+    },
 };
--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,8 @@ const ComentarioController = require('./controllers/ComentarioController');
 
 router.get('/server', ComentarioController.server);
 
+router.get('/comentarios', ComentarioController.all);
+router.post('/comentario', ComentarioController.new);
+router.get('/comentario/:id', ComentarioController.listen);
+
 module.exports = router;
